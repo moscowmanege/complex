b7322653314f7697e27de97a24c7e26b8e67a363
ponyspy <desade4me@gmail.com>
2015-11-02 23:49:46 +0300

    test

--- routes/monitors/_monitors.js
+++ routes/monitors/_monitors.js
@@ -3,7 +3,7 @@ var express = require('express');
 var Model = require(__app_root + '/models/main.js');
 
 var monitors = {
-	index: require('./index.js')
+	index: require('./index.js')(Model)
 };
 
 module.exports = (function() {
@@ -11,6 +11,9 @@ module.exports = (function() {
 
 	router.route('/')
 		.get(monitors.index.index);
+
+	router.route('/test')
+		.get(monitors.index.test);
 
 	return router;
 })();
--- routes/monitors/index.js
+++ routes/monitors/index.js
@@ -1,3 +1,39 @@
-exports.index = function(req, res) {
-  res.render('monitors/index.jade');
+module.exports = function(Model) {
+	var Event = Model.Event;
+	var module = {};
+
+	module.index = function(req, res) {
+	  res.render('monitors/index.jade');
+	}
+
+
+	module.test = function(req, res) {
+		Event.aggregate()
+			// .match({
+			// 	'area': area_id,
+			// 	'type': 'exhibits'
+			// })
+			.unwind('tickets.ids')
+			.group({
+				'_id': {
+					ticket: '$tickets.ids',
+				},
+				'events': {
+					$push: {
+						title: '$title.value',
+						description: '$description.value',
+					}
+				}
+			})
+			.exec(function(err, result) {
+				// Event.populate(result, {path: '_id.ticket', model: 'Ticket'}, function(err, events) {
+				// 	res.send(events);
+				// });
+				res.send(result);
+			})
+
+	}
+
+
+	return module;
 }
