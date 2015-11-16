5f09bedd42f2ee05a8b6a409fc12def1f1e3bcaf
ponyspy <desade4me@gmail.com>
2015-11-16 19:47:40 +0300

    init

---
+++ public/src/js/upload.js
@@ -0,0 +1,53 @@
+$(document).ready(function() {
+	$('.images_upload_preview').sortable({
+		placeholder: 'placeholder',
+		cancel: '.image_description, .image_delete',
+		sort: function(e) {
+			$('.image_description').removeClass('show');
+		}
+	});
+
+	$(document).on('click', '.image_upload_preview', function() {
+		$('.image_description').removeClass('show');
+		$(this).children('.image_description').addClass('show');
+	});
+
+	$(document).on('click', '.image_upload_preview > .image_delete', function(event) {
+		$(this).parent('.image_upload_preview').remove();
+	});
+
+	$('.images_upload_preview').filedrop({
+		url: '/preview',
+		paramname: 'image',
+		// fallback_id: 'images_upload_fallback',
+		allowedfiletypes: ['image/jpeg','image/png','image/gif'],
+		allowedfileextensions: ['.jpg','.jpeg','.png','.gif'],
+		maxfiles: 5,
+		maxfilesize: 12,
+		dragOver: function() {
+			$(this).css('outline', '2px solid red');
+		},
+		dragLeave: function() {
+			$(this).css('outline', 'none');
+		},
+		uploadStarted: function(i, file, len) {
+
+		},
+		uploadFinished: function(i, file, response, time) {
+			var image = $('<div />', {'class': 'image_upload_preview', 'style': 'background-image:url(' + response + ')'});
+			var image_delete = $('<div />', {'class': 'image_delete', 'text': 'Удалить'});
+			var image_description = $('<div />', {'class': 'image_description'});
+			var desc_ru = $('<textarea />', {'class': 'image_description_input ru_img', 'name': 'images[description][ru][]', 'placeholder':'Описание'});
+			var desc_en = $('<textarea />', {'class': 'image_description_input en_img', 'name': 'images[description][en][]', 'disabled':'disabled', 'placeholder':'Description'});
+			var image_form = $('<input />', {'class': 'image_form', 'type': 'hidden', 'name': 'images[path][]', 'value': response});
+			$('.form_images_upload').append(image.append(image_delete, image_form, image_description.append(desc_ru, desc_en)));
+		},
+		progressUpdated: function(i, file, progress) {
+
+		},
+		afterAll: function() {
+			$('.form_images_upload').css('outline', 'none');
+		}
+	});
+
+});
