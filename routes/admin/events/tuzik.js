1a2dcb04ee3ab57b0527fb41581a8650461dd19c
ponyspy <desade4me@gmail.com>
2015-11-19 22:29:25 +0300

    desc images

--- routes/admin/events/tuzik.js
+++ routes/admin/events/tuzik.js
@@ -2,12 +2,14 @@ var jsdom = require('jsdom');
 var request = require('request');
 var async = require('async');
 var fs = require('fs');
-var gm = require('gm');
+var gm = require('gm').subClass({ imageMagick: true });
+var mkdirp = require('mkdirp');
 
 
 module.exports = function(Model) {
 	var Event = Model.Event;
 	var module = {};
+	var public_path = __app_root + '/public';
 
 	var parseFields = function(window, callback) {
 		var $ = window.$;
@@ -15,7 +17,8 @@ module.exports = function(Model) {
 		var title = $('h1');
 		var s_title = $('#subheader');
 		var description = $('div[style*="margin-bottom:15px"]').next('div');
-		var images = description.find('.ms-slide');
+		var desc_images = description.find('.ms-slide');
+		var main_image = $('#SocialNetworks').children('a.facebook');
 		description.find('.master-slider-parent').remove();
 
 		var val_title = title.text();
@@ -27,14 +30,43 @@ module.exports = function(Model) {
 			.replace(/<strong><\/strong\>/g, '')
 			.replace(/strong/g, 'b');
 
-		var val_images = images.map(function() {
+		var val_main_image = main_image.attr('href').split('=')[5];
+
+		var val_desc_images = desc_images.map(function() {
 			var obj = {};
 			obj.path = $(this).children('[data-src]').attr('data-src');
 			obj.description = $(this).children('.ms-info').text();
+
 			return obj;
+
 		}).toArray();
 
-		callback(null, {title: val_title, s_title: val_s_tile, description: val_description, images: val_images});
+		callback(null, {title: val_title, s_title: val_s_tile, description: val_description, main_image: val_main_image, images: val_desc_images});
+	};
+
+	var loadImages = function(images, id, callback) {
+		var path = {
+			original: '/events/' + id + '/original/',
+			thumb: '/events/' + id + '/thumb/'
+		}
+		async.concatSeries([public_path + path.original, public_path + path.thumb], mkdirp, function(err, dirs) {
+			async.mapSeries(images, function(image, callback) {
+				var uri = encodeURI(image.path);
+				var stream = request(uri);
+				var img_name = Date.now() + '.jpg';
+
+					gm(stream).resize(1200, false).write(public_path + path.original + img_name, function (err) {
+						gm(public_path + path.original + img_name).resize(520, false).write(public_path + path.thumb + img_name, function (err) {
+						var obj = {
+							original: path.original + img_name,
+							thumb: path.thumb + img_name,
+							description: [{'lg': 'ru', 'value': image.description}]
+						};
+						callback(null, obj);
+					});
+				});
+			}, callback);
+		});
 	};
 
 	var getFields = function(locale, event, link, callback) {
@@ -67,8 +99,11 @@ module.exports = function(Model) {
 				getFields('en', event, req.body.link, callback);
 			}
 		}, function(err, result) {
-			event.save(function() {
-				res.send('ok');
+			loadImages(result.ru.fields.images, event._id, function(err, images) {
+				event.images = event.images.concat(images);
+				event.save(function() {
+					res.send('ok');
+				});
 			});
 		});
 
