bf21f5badc5ae4e062fde7b8188bbe1512e6ca39
ponyspy <desade4me@gmail.com>
2015-11-20 22:10:09 +0300

    tuzik validate

--- public/src/js/tuzik.js
+++ public/src/js/tuzik.js
@@ -9,10 +9,27 @@ $(document).ready(function() {
 		link.ru = $('.tuzik_input.rus').val();
 		link.en = $('.tuzik_input.eng').val();
 
-		$(this).off();
-		$('.tuzik_desc').text(' -- Ждем...')
-		$.post('/events/tuzik', {link: link}).done(function(data) {
-			document.location.reload();
-		});
+		var url = /moscowmanege.ru/;
+		var def_val = $('.tuzik_desc').html();
+
+		if (link.ru == '' && link.en == '') {
+			$('.tuzik_desc').text(' -- Может укажешь адрес?');
+			setTimeout(function() {
+				$('.tuzik_desc').empty().append(def_val);
+			}, 2000);
+			return false;
+		} else if (!url.test(link.ru) || url.test(link.en)) {
+			$('.tuzik_desc').text(' -- Ты не обманешь Тузика! Тузик понимает только ссылки: http://moscowmanege.ru');
+			setTimeout(function() {
+				$('.tuzik_desc').empty().append(def_val);
+			}, 4000);
+			return false;
+		} else {
+			$(this).prop('disabled', true).off();
+			$('.tuzik_desc').text(' -- Тузик думает...');
+			$.post('/events/tuzik', {link: link}).done(function(data) {
+				document.location.reload();
+			});
+		}
 	});
 });
