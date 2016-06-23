8fe0c3435778e347fa75cf57360237264d2593e5
ponyspy <desade4me@gmail.com>
2016-06-23 10:54:20 +0300

    err

--- apps/monitors/routes/_error.js
+++ apps/monitors/routes/_error.js
@@ -1,40 +1,41 @@
-exports.err_500 = function(err, req, res, next) {
-	var status = err.status || 500;
+var ErrRespond = function(req, res, status, message) {
 	res.status(status);
-
-	res.render('error', { status: status });
-
-	console.error(' ');
-	console.error('-------------');
-	console.error('*** Error ***');
-	console.error('-------------');
-	console.error(' ');
-	console.error(' ');
-	console.error('--- Url:');
-	console.error(req.protocol + '://' + req.hostname + (process.env.PORT && (':' + process.env.PORT)) + req.originalUrl);
-	console.error(' ');
-	console.error('--- Stack:');
-	console.error(err.stack);  // err.message
-	console.error(' ');
-
-};
-
-exports.err_404 = function(req, res, next) {
-	res.status(404);
 
 	// respond with html page
 	if (req.accepts('html')) {
-		res.render('error', { status: 404 });
+		res.render('error', { status: status });
 		return;
 	}
 
 	// respond with json
 	if (req.accepts('json')) {
-		res.json({ error: { status: 404, message: 'Not found' } });
+		res.json({ error: { status: status, message: message } });
 		return;
 	}
 
 	// default to plain-text
-	res.type('txt').send('404 Not found');
+	res.type('txt').send(status + ' ' + message);
 
+};
+
+exports.err_500 = function(err, req, res, next) {
+
+	[
+		' ',
+		'-------------',
+		'*** ERROR ***',
+		'-------------',
+		' ',
+		'--- Method: ' + req.method,
+		'--- Url: ' + req.protocol + '://' + req.hostname + ((process.env.NODE_ENV != 'production') && (':' + process.env.PORT)) + req.originalUrl,
+		'--- Stack:',
+		' ',
+		err.stack, // err.message
+	].forEach(function(str) { console.error(str); });
+
+	ErrRespond(req, res, err.status || 500, 'Bad params');
+};
+
+exports.err_404 = function(req, res, next) {
+	ErrRespond(req, res, 404, 'Not found');
 };
