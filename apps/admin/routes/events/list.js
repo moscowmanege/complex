6b8d7c5561661103bec4e42c5b486c82230c2ae8
ponyspy <desade4me@gmail.com>
2016-06-30 15:29:57 +0300

    i18n

--- apps/admin/routes/events/list.js
+++ apps/admin/routes/events/list.js
@@ -1,8 +1,18 @@
 var jade = require('jade');
+var i18n = require('i18n');
+
 
 module.exports = function(Model) {
 	var Event = Model.Event;
   var module = {};
+
+	var i18n_locale = function() {
+		return i18n.__.apply(null, arguments);
+	};
+
+	var i18n_plurals_locale = function() {
+		return i18n.__n.apply(null, arguments);
+	};
 
 
 	module.index = function(req, res) {
@@ -32,7 +42,7 @@ module.exports = function(Model) {
 
 		Query.sort('-date').skip(+post.context.skip).limit(+post.context.limit).exec(function(err, events) {
 			if (events && events.length > 0) {
-				var opts = {events: events, compileDebug: false, debug: false, cache: false, pretty: false};
+				var opts = {events: events, __: i18n_locale, __n: i18n_plurals_locale, compileDebug: false, debug: false, cache: false, pretty: false};
 				res.send(jade.renderFile(__app_root + '/apps/admin/views/events/_events.jade', opts));
 			} else {
 				res.send('end');
