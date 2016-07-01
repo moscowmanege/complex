df87fc64dd82ea661788cd253b99c6f9e2997a4f
ponyspy <desade4me@gmail.com>
2016-07-01 18:06:09 +0300

    catrgorys

--- apps/admin/routes/categorys/list.js
+++ apps/admin/routes/categorys/list.js
@@ -1,11 +1,52 @@
+var jade = require('jade');
+var i18n = require('i18n');
+
+
 module.exports = function(Model) {
 	var Category = Model.Category;
 	var module = {};
 
+	var i18n_locale = function() {
+		return i18n.__.apply(null, arguments);
+	};
+
+	var i18n_plurals_locale = function() {
+		return i18n.__n.apply(null, arguments);
+	};
+
 
 	module.index = function(req, res) {
-		Category.find().sort('-date').exec(function(err, categorys) {
+		Category.find().sort('-date').limit(10).exec(function(err, categorys) {
 			res.render('categorys', {categorys: categorys});
+		});
+	};
+
+	module.get_list = function(req, res) {
+		var post = req.body;
+
+		var Query = (post.context.text && post.context.text !== '')
+			? Category.find({ $text : { $search : post.context.text } } )
+			: Category.find();
+
+		if (post.context.type && post.context.type != 'all') {
+			Query.where('type').equals(post.context.type);
+		}
+
+		if (post.context.status && post.context.status == 'default') {
+			Query.where('status').ne('hidden');
+		}
+
+		if (post.context.status && post.context.status == 'hidden') {
+			Query.where('status').equals('hidden');
+		}
+
+		Query.sort('-date').skip(+post.context.skip).limit(+post.context.limit).exec(function(err, categorys) {
+			if (categorys && categorys.length > 0) {
+				var opts = {categorys: categorys, __: i18n_locale, __n: i18n_plurals_locale, compileDebug: false, debug: false, cache: false, pretty: false};
+				res.send(jade.renderFile(__app_root + '/apps/admin/views/categorys/_categorys.jade', opts));
+			} else {
+				res.send('end');
+			}
 		});
 	};
 
