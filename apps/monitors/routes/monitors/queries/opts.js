bfbde4b8ee79ed890378514f33594e09994899f1
ponyspy <desade4me@gmail.com>
2016-07-30 14:32:04 +0300

    populate

--- apps/monitors/routes/monitors/queries/opts.js
+++ apps/monitors/routes/monitors/queries/opts.js
@@ -1,11 +1,10 @@
 var Model = require(__app_root + '/models/main.js');
 
 module.exports.Populate = function(areas, callback) {
-	var Event = Model.Event;
 	var Area = Model.Area;
-	var News = Model.News;
 
 	var paths = [
+		{path: 'area', model: 'Area'},
 		{path:'complex', select: 'type price _id', model: 'Ticket'},
 		{path:'news.categorys', select: 'title', model: 'Category'},
 		{path:'events.halls', select: 'title', model: 'Hall'},
@@ -14,9 +13,7 @@ module.exports.Populate = function(areas, callback) {
 		{path:'events.tickets.ids', select: 'type price _id', model: 'Ticket'},
 	];
 
-	Event.populate(areas, paths, function(err, areas) {
-		Area.populate(areas, {path: 'area', model: 'Area'}, function(err, areas) {
-			callback(null, areas);
-		});
+	Area.populate(areas, paths, function(err, areas) {
+		callback(null, areas);
 	});
 };
