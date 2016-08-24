8fccb972b59960b50dd1b41d0b67264b1a2071d4
ponyspy <desade4me@gmail.com>
2016-08-24 19:16:54 +0300

    fix

--- apps/admin/routes/program/list.js
+++ apps/admin/routes/program/list.js
@@ -10,7 +10,11 @@ module.exports = function(Model) {
 		Event.findById(event_id).populate('program.children').exec(function(err, event) {
 			if (err) return next(err);
 
-			Event.find().where('_id').ne(event_id).where('program.children').size(0).where('program.parent').exists(false).sort('-date').exec(function(err, events) {
+			Event.find()
+			.where('_id').ne(event_id)
+			.where('meta').exists(false)
+			.where('program.children').size(0)
+			.where('program.parent').exists(false).sort('-date').exec(function(err, events) {
 				if (err) return next(err);
 
 				res.render('program', {event: event, events: events});
